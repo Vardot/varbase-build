cbc2d14865f42e6029b32eff7247dbee8c710edb
Rajab Natshah <rajabn@gmail.com>
2017-03-05 14:47:29 +0200

    Add alternative robots.txt file for non-production url with a optimized post Drupal Scaffold Procedure #18

--- scripts/varbase/Procedures.php
+++ scripts/varbase/Procedures.php
@@ -35,6 +35,38 @@ class Procedures {
     // Generated the new varbase info into the varbase info file.
     Procedures::updateProfileInfoFile($event);
   }
+  
+  /**
+   * Post Drupal Scaffold Procedure.
+   *
+   * @param \Composer\EventDispatcher\Event $event
+   *   The script event.
+   */
+  public static function postDrupalScaffoldProcedure(\Composer\EventDispatcher\Event $event) {
+    
+    $fs = new Filesystem();
+    $root = static::getDrupalRoot(getcwd());
+
+    if ($fs->exists($root . '/profiles/varbase/src/assets/robots-staging.txt')) {
+      //Create staging robots file
+      copy($root . '/profiles/varbase/src/assets/robots-staging.txt', $root . '/robots-staging.txt');
+    }
+    
+    if ($fs->exists($root . '/.htaccess')) {
+      //Alter .htaccess file
+      $htaccess_path = $root . '/.htaccess';
+      $htaccess_lines = file($htaccess_path);
+      $lines = [];
+      foreach ($htaccess_lines as $line) {
+        $lines[] = $line;
+        if (strpos($line, "RewriteEngine on") !== FALSE
+          && $fs->exists($root . '/profiles/varbase/src/assets/htaccess_extra')) {
+          $lines = array_merge($lines, file($root . '/profiles/varbase/src/assets/htaccess_extra'));
+        }
+      }
+      file_put_contents($htaccess_path, $lines);
+    }
+  }
 
   /**
    * Post update procedure.
