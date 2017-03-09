d78ecb44bc07781da9f8e3fcd4bc86797f82323a
Rajab Natshah <rajabn@gmail.com>
2017-03-09 15:17:11 +0200

    Manage Varbase and Varbase features versions in the Post Install, Post Update, Post Scaffold #19

--- scripts/varbase/Procedures.php
+++ scripts/varbase/Procedures.php
@@ -89,35 +89,81 @@ class Procedures {
     $fs = new Filesystem();
     $root = static::getDrupalRoot(getcwd());
 
-    // Full path and file name for the varbase.info.yml file.
-    $varbase_info_file = $root . '/profiles/varbase/varbase.info.yml';
+    // File name for the varbase.info.yml file.
+    $varbase_info_file = '/profiles/varbase/varbase.info.yml';
+    $varbase_info_file_with_root_path = $root . $varbase_info_file;
 
-    if ($fs->exists($varbase_info_file)) {
+    // File names for varbase features info files.
+    $varbase_features_info_files = array(
+      'varbase_admin' => '/profiles/varbase/modules/varbase_features/varbase_admin/varbase_admin.info.yml',
+      'varbase_core' => '/profiles/varbase/modules/varbase_features/varbase_core/varbase_core.info.yml',
+      'varbase_development' => '/profiles/varbase/modules/varbase_features/varbase_development/varbase_development.info.yml',
+      'varbase_internationalization' => '/profiles/varbase/modules/varbase_features/varbase_internationalization/varbase_internationalization.info.yml',
+      'varbase_landing' => '/profiles/varbase/modules/varbase_features/varbase_landing/varbase_landing.info.yml',
+      'varbase_media' => '/profiles/varbase/modules/varbase_features/varbase_media/varbase_media.info.yml',
+      'varbase_page' => '/profiles/varbase/modules/varbase_features/varbase_page/varbase_page.info.yml',
+      'varbase_security' => '/profiles/varbase/modules/varbase_features/varbase_security/varbase_security.info.yml',
+      'varbase_seo' => '/profiles/varbase/modules/varbase_features/varbase_seo/varbase_seo.info.yml',
+      'varbase_site' => '/profiles/varbase/modules/varbase_features/varbase_site/varbase_site.info.yml',
+      'varbase_user' => '/profiles/varbase/modules/varbase_features/varbase_user/varbase_user.info.yml',
+    );
+
+    if ($fs->exists($varbase_info_file_with_root_path)) {
+      $varbase_info_datestamp = time();
       // Parse the varbase.info.yml file.
-      $varbase_info = Yaml::parse(file_get_contents($varbase_info_file));
+      $varbase_info = Yaml::parse(file_get_contents($varbase_info_file_with_root_path));
 
-      // Get varbase version.
-      $varbase_version = $event->getComposer()
+      // Varbase version.
+      $varbase_version = '8.x-4.x-dev';
+      $varbase_package = $event->getComposer()
         ->getRepositoryManager()
         ->getLocalRepository()
-        ->findPackage('vardot/varbase', "*")
-        ->getVersion();
+        ->findPackage('vardot/varbase', "*");
+      
+      // Only get the version if it was not DEV. to follow Drupal standard.
+      if (!$varbase_package->isDev()) {
+        $varbase_version = $varbase_package->getVersion();
+      }
 
       // Information added by varbase-build packaging script.
       $varbase_info['version'] = $varbase_version;
       $varbase_info['project'] = 'varbase';
-      $varbase_info['datestamp'] = time();
+      $varbase_info['datestamp'] = $varbase_info_datestamp;
 
       // Dump the array to string of Yaml format.
       $new_varbase_info = Yaml::dump($varbase_info);
 
       // Save the new varbase info into the varbase info file.
-      file_put_contents($varbase_info_file, $new_varbase_info);
+      file_put_contents($varbase_info_file_with_root_path, $new_varbase_info);
 
       // Print out a message to the user on Install and update.
       $event->getIO()->write(" Information added by varbase-build packaging script.");
-      $event->getIO()->write(" - Updated varbase.info.yml file.");
       $event->getIO()->write(" - version: " . $varbase_version);
+      $event->getIO()->write(" - datestamp: " . $varbase_info_datestamp);
+      $event->getIO()->write(" - Updated files.");
+      $event->getIO()->write($varbase_info_file);
+      
+      // Update all varbase features info.yml files.
+      foreach ($varbase_features_info_files as $varbase_feature_info_file) {
+        $varbase_feature_info_file_with_root_path = $root . $varbase_feature_info_file;
+        if ($fs->exists($varbase_feature_info_file_with_root_path)) {
+          // Parse the varbase feature info.yml file.
+          $varbase_feature_info = Yaml::parse(file_get_contents($varbase_feature_info_file_with_root_path));
+
+          // Information added by varbase-build packaging script.
+          $varbase_feature_info['version'] = $varbase_version;
+          $varbase_feature_info['datestamp'] = $varbase_info_datestamp;
+
+          // Dump the array to string of Yaml format.
+          $new_varbase_feature_info = Yaml::dump($varbase_feature_info);
+
+          // Save the new varbase feature info into the info.yml file.
+          file_put_contents($varbase_feature_info_file_with_root_path, $new_varbase_feature_info);
+
+          // Print out a message to the user on Install and update.
+          $event->getIO()->write($varbase_feature_info_file);
+        }
+      }
     }
   }
 }
