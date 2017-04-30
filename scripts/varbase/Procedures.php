75f77e2813bddefcf0573a050d4d7a7133a3aeb6
Rajab Natshah <rajabn@gmail.com>
2017-05-01 00:25:11 +0300

    Issue #2874207: Updated [Varbase Editor] feature from 8.4.6 to 8.4.7

--- scripts/varbase/Procedures.php
+++ scripts/varbase/Procedures.php
@@ -83,6 +83,11 @@ class Procedures {
       // Alter development.services.yml to have Varbase's Local development services.
       copy($root . '/profiles/varbase/src/assets/development.services.yml', $root . '/sites/development.services.yml');
     }
+    
+    if ($fs->exists($root . '/libraries/ace/src-min-noconflict/ace.js')) {
+      mkdir($root . '/modules/contrib/ace_editor/libraries', 0777, true);
+      rename($root . '/libraries/ace', $root . '/modules/contrib/ace_editor/libraries/ace');
+    }
   }
   
   /**
